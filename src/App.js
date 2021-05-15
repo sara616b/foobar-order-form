7bba45009933362c05fe03b429b2545c357eed47
Sarah <sara616b@stud.kea.dk>
2021-05-15 11:26:31 +0200

    fetching data from heroku

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
-import "./BeerForProductList";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import BeerForProductList from "./BeerForProductList";
 import Header from "./Header";
 import Footer from "./Footer";
 import BackButton from "./BackButton";
 import Button from "./Button";
 import ProductView from "./ProductView";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import BasketView from "./BasketView";
 import PaymentView from "./PaymentView";
 import ThankYouView from "./ThankYouView";
 
 function App() {
+  const [beerTypes, setBeerTypes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://foobar-vas.herokuapp.com/beertypes")
+      .then((res) => res.json())
+      .then((data) => {
+        setBeerTypes(data);
+      });
+  }, []);
+
+  console.log(beerTypes);
+
   return (
     <Router>
       <div className="App">

c0289254109f63ca9e8248b9c4509cca35fa9adc
Victoria <vigi2586@stud.kea.dk>
2021-05-25 16:17:28 +0200

    validate on pay form done

--- src/PaymentView.js
+++ src/PaymentView.js
@@ -110,10 +110,11 @@ export default function PaymentView({
                 maxLength="19"
                 min="0000 0000 0000 0000"
                 max="9999 9999 9999 9999"
+                pattern="[a-zA-Z ]-"
                 required
               />
               <span className="error" id="err-name" aria-live="assertive">
-                Can't be more or less than 16 numbers
+                Can't be letters and must be 16 numbers
               </span>
             </label>
 
@@ -141,10 +142,13 @@ export default function PaymentView({
                 id="expirationDate"
                 autoComplete="xyz"
                 placeholder="2021-06-04"
-                min="2021-01-01"
-                max="2028-12-31"
+                pattern="((?:20|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))"
                 required
               />
+
+              <span className="error" id="err-name" aria-live="assertive">
+                The date must be between 2019 - 2028
+              </span>
             </label>
 
             <label htmlFor="cvv">
@@ -159,7 +163,7 @@ export default function PaymentView({
                 required
               />
               <span className="error" id="err-name" aria-live="assertive">
-                Must be a valid number
+                Can't be letters, must be 3 numbers long
               </span>
             </label>
 
